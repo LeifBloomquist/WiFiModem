e21164cc382c073684870b127890666aa8a27f99
Leif Bloomquist <github@leifbloomquist.net>
2016-10-08 14:54:02 -0400

    C128 / MacOS tools

--- MicroView/WiFiModem-Menu/__vm/.WiFiModem-Menu.vsarduino.h
+++ MicroView/WiFiModem-Menu/__vm/.WiFiModem-Menu.vsarduino.h
@@ -5,7 +5,7 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Uno, Platform=avr, Package=arduino
+	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
@@ -13,10 +13,11 @@
 #define __AVR_ATmega328p__
 #define __AVR_ATmega328P__
 #define F_CPU 16000000L
-#define ARDUINO 176
-#define ARDUINO_AVR_UNO
+#define ARDUINO 10608
+#define ARDUINO_AVR_NANO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus
+#define __AVR__
 #define __inline__
 #define __asm__(x)
 #define __extension__
@@ -83,7 +84,7 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portOutputRegister(P)
 #define portInputRegister(P)
 #define portModeRegister(P)
-#include <WiFiModem-Menu.ino>
-#include <petscii.cpp>
-#include <petscii.h>
+#include <..\WiFiModem-Menu\WiFiModem-Menu.ino>
+#include <..\WiFiModem-Menu\petscii.cpp>
+#include <..\WiFiModem-Menu\petscii.h>
 #endif
